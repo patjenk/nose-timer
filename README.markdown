c703fbce210ec8fed8cdf337f0af3c883852a4e6
Patrick Jenkins <patjenk@gmail.com>
2011-05-19 23:14:46 +0000

    Adding the actual guts of the code and files necessary to install this with PIP.

--- README.markdown
+++ README.markdown
@@ -2,4 +2,4 @@
 
 ## nose-timer
 
-@mahmoudimus wrote this timer for nose tests and I want to use it in our project. So I'm creating a repository so I can install it with PIP. 
+@mahmoudimus wrote this timer for nose tests and I want to use it in our project. So I'm creating a repository so I can install it with PIP. The original gist can be viewed in all it's glory at https://gist.github.com/848183.
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+version = __import__('nose-timer').__version__
+
+setup(
+    name='nose-timer',
+    version=version,
+    description="A python nose plugin to time tests.",
+    long_description="This plugin provides test timings to identify which tests might be taking the most. From this information, it might be useful to couple individual tests nose's `--with-profile` option to profile problematic tests.",
+    author='Mahmoud Abdelkader',
+    author_email='',
+    url='https://github.com/patjenk/nose-timer',
+    install_requires=[
+        'werkzeug>=0.6.2',
+        'mako>=0.3.6',
+        'simplejson',
+        'mock',
+        'nose==1.0.0',
+        'boto>=2.0a2',
+        'SQLAlchemy>=0.6.3',
+    ],
+    setup_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='nose.collector',
+    zip_safe=False,
+    entry_points={
+        'nose.plugins.0.10': [
+            'with-test-timers = application.utils.test_timer:TestTimer',
+        ]
+    },
+)
